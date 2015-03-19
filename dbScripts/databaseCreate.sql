70a7c1a19421088a2ef52b1a3203b9c320696c1a
DNguyen17 <danhn@smu.edu>
2015-03-19 00:56:20 -0500

    updated db name with error log

--- dbScripts/databaseCreate.sql
+++ dbScripts/databaseCreate.sql
@@ -5,19 +5,19 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema stablestudy
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema stablestudy
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `stablestudy` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `stablestudy` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`user`
+-- Table `stablestudy`.`user`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`user` (
+CREATE TABLE IF NOT EXISTS `stablestudy`.`user` (
   `username` VARCHAR(20) NOT NULL,
   `email` VARCHAR(30) NULL,
   `namef` VARCHAR(20) NULL,
@@ -31,9 +31,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`building`
+-- Table `stablestudy`.`building`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`building` (
+CREATE TABLE IF NOT EXISTS `stablestudy`.`building` (
   `buildingID` INT NOT NULL,
   `bName` VARCHAR(25) NULL,
   `closedAtNight` TINYINT(1) NULL,
@@ -44,9 +44,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`room`
+-- Table `stablestudy`.`room`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`room` (
+CREATE TABLE IF NOT EXISTS `stablestudy`.`room` (
   `id` INT NOT NULL,
   `number` INT NULL,
   `buidlingID:FK` INT NULL,
@@ -61,16 +61,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`room` (
   INDEX `buildingID_idx` (`buidlingID:FK` ASC),
   CONSTRAINT `buildingID`
     FOREIGN KEY (`buidlingID:FK`)
-    REFERENCES `mydb`.`building` (`buildingID`)
+    REFERENCES `stablestudy`.`building` (`buildingID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`favorites`
+-- Table `stablestudy`.`favorites`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`favorites` (
+CREATE TABLE IF NOT EXISTS `stablestudy`.`favorites` (
   `favID` VARCHAR(45) NOT NULL,
   `username` VARCHAR(20) NULL,
   `favRoom` INT NULL,
@@ -78,16 +78,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`favorites` (
   INDEX `username_idx` (`username` ASC),
   CONSTRAINT `username`
     FOREIGN KEY (`username`)
-    REFERENCES `mydb`.`user` (`username`)
+    REFERENCES `stablestudy`.`user` (`username`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`friends`
+-- Table `stablestudy`.`friends`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`friends` (
+CREATE TABLE IF NOT EXISTS `stablestudy`.`friends` (
   `friendID` INT NOT NULL,
   `user1` VARCHAR(20) NULL,
   `user2` VARCHAR(20) NULL,
@@ -97,21 +97,21 @@ CREATE TABLE IF NOT EXISTS `mydb`.`friends` (
   INDEX `user2_idx` (`user2` ASC),
   CONSTRAINT `user1`
     FOREIGN KEY (`user1`)
-    REFERENCES `mydb`.`user` (`username`)
+    REFERENCES `stablestudy`.`user` (`username`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `user2`
     FOREIGN KEY (`user2`)
-    REFERENCES `mydb`.`user` (`username`)
+    REFERENCES `stablestudy`.`user` (`username`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`comments`
+-- Table `stablestudy`.`comments`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`comments` (
+CREATE TABLE IF NOT EXISTS `stablestudy`.`comments` (
   `commentID` INT NOT NULL,
   `username` VARCHAR(20) NULL,
   `details` VARCHAR(140) NULL,
@@ -121,16 +121,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`comments` (
   INDEX `username(20)_idx` (`username` ASC),
   CONSTRAINT `username`
     FOREIGN KEY (`username`)
-    REFERENCES `mydb`.`user` (`username`)
+    REFERENCES `stablestudy`.`user` (`username`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`reservation`
+-- Table `stablestudy`.`reservation`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`reservation` (
+CREATE TABLE IF NOT EXISTS `stablestudy`.`reservation` (
   `resID` INT NOT NULL,
   `username` VARCHAR(20) NULL,
   `roomID:FK` INT NULL,
@@ -141,21 +141,21 @@ CREATE TABLE IF NOT EXISTS `mydb`.`reservation` (
   INDEX `username_idx` (`username` ASC),
   CONSTRAINT `username`
     FOREIGN KEY (`username`)
-    REFERENCES `mydb`.`user` (`username`)
+    REFERENCES `stablestudy`.`user` (`username`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `roomID`
     FOREIGN KEY (`roomID:FK`)
-    REFERENCES `mydb`.`room` (`id`)
+    REFERENCES `stablestudy`.`room` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`meetings`
+-- Table `stablestudy`.`meetings`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`meetings` (
+CREATE TABLE IF NOT EXISTS `stablestudy`.`meetings` (
   `meetingID` INT NOT NULL,
   `username` VARCHAR(20) NULL,
   `roomID` INT NULL,
@@ -166,21 +166,21 @@ CREATE TABLE IF NOT EXISTS `mydb`.`meetings` (
   INDEX `roomID_idx` (`roomID` ASC),
   CONSTRAINT `username`
     FOREIGN KEY (`username`)
-    REFERENCES `mydb`.`user` (`username`)
+    REFERENCES `stablestudy`.`user` (`username`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `roomID`
     FOREIGN KEY (`roomID`)
-    REFERENCES `mydb`.`room` (`id`)
+    REFERENCES `stablestudy`.`room` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`meetingUsers`
+-- Table `stablestudy`.`meetingUsers`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`meetingUsers` (
+CREATE TABLE IF NOT EXISTS `stablestudy`.`meetingUsers` (
   `uID` INT NOT NULL,
   `meetingID` INT NULL,
   `username` VARCHAR(20) NULL,
@@ -189,12 +189,12 @@ CREATE TABLE IF NOT EXISTS `mydb`.`meetingUsers` (
   INDEX `username_idx` (`username` ASC),
   CONSTRAINT `meetingID`
     FOREIGN KEY (`meetingID`)
-    REFERENCES `mydb`.`meetings` (`meetingID`)
+    REFERENCES `stablestudy`.`meetings` (`meetingID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `username`
     FOREIGN KEY (`username`)
-    REFERENCES `mydb`.`user` (`username`)
+    REFERENCES `stablestudy`.`user` (`username`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
