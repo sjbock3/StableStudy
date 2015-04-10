783c544f06e711314e54f0038e06002932e5be12
bcelii <bcelii@smu.edu>
2015-04-10 17:02:37 -0500

    commented out geometry and vt in index.html main page

---
+++ js/BC_dummy_classroom.js
@@ -0,0 +1,84 @@
+/**
+ * Created by Brendan on 4/10/2015.
+ */
+var dummyClassData = {
+    "capacity": null,
+    "name": "Florence 106",
+    "external_id": null,
+    "extended_info": {
+    "rating": "3.0",
+        "review_count": "1",
+        "orientation": "1st floor; near hughest trig",
+        "noise_level": "variable",
+        "location_description": "Physics/Astronomy Tower, near H-Bar",
+        "has_natural_light": "true",
+        "food_nearby": "building",
+        "campus": "SMU"
+    },
+    "uri": "/api/v1/spot/591", /*********Need to change********/
+    "available_hours": {
+    "monday": [
+        ["00:00", "23:59"]
+    ],
+        "tuesday": [
+        ["00:00", "23:59"]
+    ],
+        "friday": [
+        ["00:00", "23:59"]
+    ],
+        "wednesday": [
+        ["00:00", "23:59"]
+    ],
+        "thursday": [
+        ["00:00", "23:59"]
+    ],
+        "sunday": [
+        ["00:00", "23:59"]
+    ],
+        "saturday": [
+        ["00:00", "23:59"]
+    ]
+},
+    "manager": "",
+    "last_modified": "2014-12-01T19:35:17+00:00",
+    "etag": "01603d4ae534fbc1b329d1da04f4e8013e545b4d",
+    "type": "Outdoor area",
+    "images": [{
+    "upload_user": "cstimmel",
+    "thumbnail_root": "/api/v1/spot/591/image/1324/thumb", /*********Need to change********/
+    "modification_date": "2014-07-29T17:10:13+00:00",
+    "url": "/api/v1/spot/591/image/1324",
+    "upload_application": "spotseeker_admin_1.0",
+    "display_index": 0,
+    "height": 2000,
+    "width": 3008,
+    "creation_date": "2012-08-30T10:25:27+00:00",
+    "content-type": "image/jpeg",
+    "id": 1324,
+    "description": ""
+}, {
+    "upload_user": "cstimmel",
+    "thumbnail_root": "/api/v1/spot/591/image/1325/thumb", /*********Need to change********/
+    "modification_date": "2014-07-29T17:10:13+00:00",
+    "url": "/api/v1/spot/591/image/1325", /*********Need to change********/
+    "upload_application": "spotseeker_admin_1.0",
+    "display_index": 1,
+    "height": 2000,
+    "width": 3008,
+    "creation_date": "2012-08-30T10:26:00+00:00",
+    "content-type": "image/jpeg",
+    "id": 1325,
+    "description": ""
+}],
+    "organization": "",
+    "display_access_restrictions": "",
+    "id": 591,
+    "location": {
+    "floor": "1st floor",
+        "height_from_sea_level": null,
+        "room_number": "",
+        "longitude": -96.786349,
+        "latitude": 32.845576,
+        "building_name": "Florence"
+}
+}
