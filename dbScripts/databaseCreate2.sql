19a969733ab752b28fd0210644ad063076362df6
DNguyen17 <danhn@smu.edu>
2015-04-21 23:33:41 -0500

    updated database

--- dbScripts/databaseCreate2.sql
+++ dbScripts/databaseCreate2.sql
@@ -5,6 +5,9 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
 -- Schema stablestudy
 -- -----------------------------------------------------
 DROP SCHEMA IF EXISTS `stablestudy` ;
